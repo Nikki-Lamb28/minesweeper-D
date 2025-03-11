721d53f40a9f04c7936accc9d5507fd39f30818c
Nikki-Lamb28 <nikki.lamb28@student.tamdistrict.org>
2025-03-11 15:34:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const numRows = 20;
 const numColums = 20;
 const numBomb = 99;
 let flags = 0;
+let living = true;
 const remaningMines = numbBombs - flags;
 
 function gridd() { 
-rowss(numRows);
-columss(numcolums);
 
+const minsA  = Arrayy(numBomb).fill(mine);
+const mtA  = Array(numColums * numRows - numBomb).fill("empty");
+const gameA = mtA.contact(minsA).sort(()
+  
+  
 
 }
